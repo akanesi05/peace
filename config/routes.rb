4468b8070c32f960d14930356276f620b4227656
三井　茜 <mitsuiakane@mitsuiakanenoMacBook-Air.local>
2023-12-29 15:08:43 +0900

    トップページを作成しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  #get 'static_pages/top'
+  root 'static_pages#top'
+  #get '/top', to: 'static_pages#top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  
+  
 end
