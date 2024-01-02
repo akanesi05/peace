2df01869f35728daaa921f7a6451a56e7d7df744
三井　茜 <mitsuiakane@mitsuiakanenoMacBook-Air.local>
2024-01-02 23:47:44 +0900

    ログイン作成しました

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
+bundle exec rails db:drop db:create db:migrate DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rails db:migrate:reset
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+#bundle exec rails db:migrate
 
