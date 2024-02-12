657b6a05c5603c0f75da932790bb54bfd62e6c24
三井　茜 <mitsuiakane@mitsuiakanenoMacBook-Air.local>
2024-02-13 00:00:54 +0900

    お問合せ修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_03_122031) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_25_150242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,13 +22,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_03_122031) do
     t.index ["pose_id"], name: "index_bookmarks_on_pose_id"
     t.index ["user_id", "pose_id"], name: "index_bookmarks_on_user_id_and_pose_id", unique: true
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
-  end
-
-  create_table "contacts", force: :cascade do |t|
-    t.string "name"
-    t.text "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "poses", force: :cascade do |t|
