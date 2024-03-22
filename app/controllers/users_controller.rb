a1375c74b99a6b0dcf37a466fc8ff15f40a65794
mitsuiakane <akane.sumitomo@gmail.com>
2024-03-22 22:40:57 +0900

    プロフラジオボタン

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,17 +9,7 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  #def create
-    #@user = User.new(user_params)
-    #if @user.save
-      #redirect_to root_path
-    #else
-      #render :new
-  #end
-
-  #def show
-      #@user = User.find(params[:id])
-  #end
+  
   
   def create
     @user = User.new(user_params)
