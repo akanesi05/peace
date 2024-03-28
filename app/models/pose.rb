ed36b3a39617b56cb3ca5430cd7852dcdbcf61ca
mitsuiakane <akane.sumitomo@gmail.com>
2024-03-28 21:44:19 +0900

    バリデーション

--- app/models/pose.rb
+++ app/models/pose.rb
@@ -2,7 +2,15 @@ class Pose < ApplicationRecord
   belongs_to :user
   has_many :bookmarks, dependent: :destroy
   mount_uploader :image, ImageUploader
+  validates :name, presence: true
+  #validate :pose_is_exist
+  private
 
+  #def pose_is_exist
+      # if ! pose.where(id: pose_id).all.present?
+      #     errors.add(:pose_id, "は存在しません。")
+      # end
+  #end
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "id", "id_value", "image", "name", "updated_at", "user_id"]
   end
