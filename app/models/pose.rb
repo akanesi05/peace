5206d0dbb6f56d0190e4f76c0c3b4f1c93ad8b19
mitsuiakane <akane.sumitomo@gmail.com>
2024-03-30 21:14:37 +0900

    画像必須

--- app/models/pose.rb
+++ app/models/pose.rb
@@ -3,14 +3,12 @@ class Pose < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   mount_uploader :image, ImageUploader
   validates :name, presence: true
-  #validate :pose_is_exist
-  private
+  validate :image_presence
 
-  #def pose_is_exist
-      # if ! pose.where(id: pose_id).all.present?
-      #     errors.add(:pose_id, "は存在しません。")
-      # end
-  #end
+  def image_presence
+  errors.add(:image, "をアップロードしてください") unless image.present?
+  end
+
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "id", "id_value", "image", "name", "updated_at", "user_id"]
   end
