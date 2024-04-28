1e0a5d9311b4f45898dbd1eef0d8a89f519b8b4f
mitsuiakane <akane.sumitomo@gmail.com>
2024-04-28 22:25:06 +0900

    Flash

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,12 +2,12 @@ class BookmarksController < ApplicationController
   def create
     @pose = Pose.find(params[:pose_id])
     current_user.bookmark(@pose)
+    redirect_to poses_path, success: t('.success')  
+  end
     
-   end
-    
-   def destroy
+  def destroy
     @pose = current_user.bookmarks.find(params[:id]).pose
-        current_user.unbookmark(@pose)
-    
-      end
+    current_user.unbookmark(@pose)
+    redirect_to poses_path, success: t('.success'), status: :see_other  
+  end
 end
