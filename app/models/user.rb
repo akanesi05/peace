427a1b6c8cb3056dc556f3376cc3df0832c05d03
mitsuiakane <akane.sumitomo@gmail.com>
2024-04-30 22:54:53 +0900

    Flash2

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :name, presence: true, length: { minimum: 3 }
+  validates :name, presence: true, length: { maximum: 10 }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :reset_password_token, uniqueness: true, allow_nil: true
   def own?(object)
