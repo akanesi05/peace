9735583b534d9338565853bfc77b753b937317d1
mitsuiakane <akane.sumitomo@gmail.com>
2024-06-26 22:46:24 +0900

    いくつか予めポーズを表示できる

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,7 +2,8 @@ class TopController < ApplicationController
   skip_before_action :require_login, only: %i[index]
 
   def index
-    @pose = Pose.find_by(params[:id])
+    #@pose = Pose.find_by(params[:id])
+    @poses = Pose.all.order("created_at DESC").limit(6)
   end
 
 
