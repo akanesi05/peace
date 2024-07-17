725171306bff726f1a12469b011375dd2fd7180f
mitsuiakane <akane.sumitomo@gmail.com>
2024-07-17 19:43:48 +0900

    タグ

--- app/models/pose_tag.rb
+++ app/models/pose_tag.rb
@@ -2,5 +2,5 @@ class PoseTag < ApplicationRecord
   belongs_to :pose
   belongs_to :tag
 
-  validates :post_id, uniqueness: { scope: :tag_id }
+  validates :pose_id, uniqueness: { scope: :tag_id }
 end
