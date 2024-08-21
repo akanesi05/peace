2fa80ad0ef3eec52c84c3c1bcaefed96b1470ef0
mitsuiakane <akane.sumitomo@gmail.com>
2024-08-21 17:35:51 +0900

    ゲストの初期データを入れる、投稿の見た目

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,4 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-User.create!(name: '鈴木', email: 'falao@sample.com',password: 'cpy5sp10',password_confirmation: 'cpy5sp10')
-User.create!(name: '花子', email: 'hanako@sample.com',password: 'cpy5sp11',password_confirmation: 'cpy5sp11')
-User.create!(name: '山田', email: 'yamada@sample.com',password: 'cpy5sp12',password_confirmation: 'cpy5sp12')
-User.create!(name: '太郎', email: 'tarou@sample.com',password: 'cpy5sp13',password_confirmation: 'cpy5sp13')
-User.create!(name: '佐藤', email: 'falao@sample.com',password: 'cpy5sp14',password_confirmation: 'cpy5sp14')
-User.create!(name: '野村', email: 'nomu@sample.com',password: 'cpy5sp15',password_confirmation: 'cpy5sp15')
-User.create!(name: '菊本', email: 'bosu@sample.com',password: 'cpy5sp16',password_confirmation: 'cpy5sp16')
-User.create!(name: '中井', email: 'nakai@sample.com',password: 'cpy5sp17',password_confirmation: 'cpy5sp17')
+User.create!(name: 'ゲスト', email: 'guest@sample.com',password: 'cpy5sp10',password_confirmation: 'cpy5sp10')
