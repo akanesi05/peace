3d948e555e905b7342c2afe09e5be3bb53883a4c
mitsuiakane <akane.sumitomo@gmail.com>
2024-08-30 12:19:31 +0900

    Google認証

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :bookmark_poses, through: :bookmarks, source: :pose
   mount_uploader :avatar, AvatarUploader
+  has_many :authentications, :dependent => :destroy
+  accepts_nested_attributes_for :authentications
 
   validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
