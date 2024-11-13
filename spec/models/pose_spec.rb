cdd1ea3b58d12c63e984c58aeb1a04d147f1def2
mitsuiakane <akane.sumitomo@gmail.com>
2024-11-13 22:16:58 +0900

    異常系のポーズモデルのテスト

--- spec/models/pose_spec.rb
+++ spec/models/pose_spec.rb
@@ -4,29 +4,30 @@ RSpec.describe 'Poseモデルのテスト', type: :model do
   describe 'バリデーションのテスト' do
     # factoriesで作成したダミーデータを使用します。
     let(:user) { FactoryBot.create(:user) }
-    let!(:pose) {build(:pose, user_id: user.id) }
+    let!(:pose) {build(:pose, user_id: user.id) }#ポーズの変数を作り、13とか２０行目で使っている。
 
-   
+    before do
+      pose.image = fixture_file_upload('test.jpeg')
+    end
 
     context '正常な値の場合' do
-      it '存在チェックが効いていること' do
-        pose.image = fixture_file_upload('test.jpeg')
+      it '有効であること' do
         expect(pose.valid?).to eq true
       end
     end
+#ネームが空だと、テストが通る#無効であることがtrue画像があれば無効なデータとして扱われる　画像がないと自動的に無効になる
+#valid?が有効だったらtrueを返すメソッド,invalid？が無効だったらtrue
+    context '異常な値の場合' do
+      it 'ネームが空だと無効であること' do
+        pose.name = ""
+        expect(pose.invalid?).to eq true
+      end
+    end
+    context '異常な値の場合' do
+      it '画像が空だと無効であること' do
+        pose.image = ""
+        expect(pose.invalid?).to eq true
+      end
+    end
   end
-  # describe 'アソシエーションのテスト' do
-  #   context 'customerモデルとの関係' do
-  #     it 'N:1となっている' do
-  #       expect(Pose.reflect_on_association(:user).macro).to eq :belongs_to
-  #     end
-  #   end
-
-  #   # has_manyの関係性で記述するのもありです。
-  #   # context 'PoseCommentモデルとの関係' do
-  #     # it '1:Nとなっている' do
-  #       # expect(Pose.reflect_on_association(:pose_comments).macro).to eq :has_many
-  #     # end
-  #   # end
-  # end
 end
